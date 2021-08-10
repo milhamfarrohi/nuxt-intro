843a93c407814808850368b89a3247feab9d9897
M Ilham Farrohi <ilham@alterra.id>
2021-08-10 08:52:24 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardSections in largeCardSections"
+      :key="cardSections.id"
+      :cardsSection="cardSections"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from '@/assets/data.js'
+export default {
+  data () {
+    return {
+      largeCardSections
+    }
+  }
+}
+</script>
